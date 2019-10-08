905be5b3f4323dc266b3a90f4ddda51a67682939
wikauspina <wikauspina@gmail.com>
2019-10-08 21:57:52 +0700

    menambahkan file produk.php

--- coba.php
+++ coba.php
@@ -11,3 +11,5 @@ $b = new Coba();
 $c = new Coba();
 
 var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ produk.php
@@ -0,0 +1,15 @@
+<?php 
+
+//jualan produk
+
+//komil
+
+//game
+class Produk {
+
+}
+
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+$produk3 = new Produk();
